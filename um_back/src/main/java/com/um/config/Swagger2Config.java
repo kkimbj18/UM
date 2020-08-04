98378ba5a44994a903bde3cdb8de272d38c408e2
JungHun <youngsangsid@naver.com>
2020-08-04 20:28:45 +0900

    swagger set up

---
+++ um_back/src/main/java/com/um/config/Swagger2Config.java
@@ -0,0 +1,24 @@
+package com.um.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                //.host("ec2-3-34-84-81.ap-northeast-2.compute.amazonaws.com:8085")
+                .host("localhost:8085")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
